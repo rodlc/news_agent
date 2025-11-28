185fc351282389e105c22a68fc9f1b10226792ea
Jennifer Kiu <160426229+JenniferAliceKiu@users.noreply.github.com>
2025-11-28 12:12:21 +0100

    Seed update (#17)
    * Scraper works, seed pending, you need key
    
    * Seed works again, gemfile too, and Scraper works
    
    * Delete app/agents/claude.md
    
    * Delete app/agents/m1_news_scraper.rb
    
    * Delete app/agents/readme_scraper.md
    
    ---------
    
    Co-authored-by: Rodolphe L. <63702959+rodlc@users.noreply.github.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,58 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+puts "Cleaning database..."
+
+#Chat.destroy_all
+Message.destroy_all
+Chat.destroy_all
+Daily.destroy_all
+
+User.destroy_all
+
+
+puts 'Populating database...'
+puts 'Creating users'
+
+
+users = User.new(
+    email: Faker::Internet.email,
+    password: 'password123'
+  )
+  users.save!
+puts 'Users done'
+
+puts 'Creating dailies'
+10.times do
+  #daily = ''
+  dailies = Daily.new(
+    title: Faker::Lorem.sentence(word_count: 5),
+    summary: Faker::Lorem.paragraph(sentence_count: 3)
+  )
+  dailies.save!
+  end
+puts 'Dailies done'
+
+### Put to singular @Rodolphe
+
+puts 'creating chats'
+10.times do
+  chats = Chat.new(
+    name: Faker::Lorem.words(number: 3).join(' '),
+    user: User.last,
+    daily: Daily.last
+  )
+  chats.save!
+  end
+puts 'Chats done'
+
+
+puts 'Creating messages'
+25.times do
+  messages = Message.new(
+    content: Faker::Lorem.sentence(word_count: 10),
+    direction: ['incoming', 'outgoing'].sample,
+    chat: Chat.last
+  )
+  messages.save!
+  end
+puts 'Messages done'
